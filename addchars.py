071c1d0232b2895055ba83f3e8c739f4152ea9ec
Bobby de Vos <bobby_devos@sil.org>
2019-01-02 16:54:33 -0700

    Refactor adding characters in bulk

--- addchars.py
+++ addchars.py
@@ -1,51 +1,21 @@
 #!/bin/python
 
-import os
-import os.path
-import sys
-
-from wscript import *
-
-charis_dir = '../../../latn/fonts/charis_local/5.000/zip/unhinted/'
-charis_ttf = '/CharisSIL'
-gentium_dir = '../../../latn/fonts/gentium_local/basic/1.102/zip/unhinted/'
-gentium_ttf = '/GenBkBas'
-annapurna_dir = '../../../deva/fonts/annapurna_local/1.203/zip/unhinted/'
-annapurna_ttf = '/AnnapurnaSIL-'
-panini = '../../../deva/fonts/panini-master/source/Panini'
-deva = '../../../deva/fonts/panini/source/'
-thiruvalluvar = '../../../taml/fonts/thiruvalluvar/source/ThiruValluvar'
-vaigai = '../../../taml/fonts/thiruvalluvar/source/Vaigai'
-exo = '../../../latn/fonts/exo/1.500/zip/unhinted/1000/Exo-'
-
-def runCommand(cmd, ifont, ofont):
-    cmd = 'ffcopyglyphs' + ' -f ' + cmd + ' ' + ifont + ' ' + ofont
-    print cmd
-    os.system(cmd)
-
-def findFile(filename):
-    return os.path.join(sys.argv[1], filename)
-
-def modifyFile(cmd, filename):
-    tmp = 'tmp.sfd'
-    os.rename(findFile(filename), tmp)
-    runCommand(cmd, tmp, findFile(filename))
-    os.remove(tmp)
+from addcharslib import *
 
 def modifySource(sfd, f, s, sn):
     print sfd
 
-    emsize = '1000'
-    emext = '.sfd'
-    emopt = '-s ' + str(1000.0/2048/1.4) + ' '
+    workshop = 1.4
+    upm = 1000.0/2048.0
+    scale = '-s ' + str(upm/workshop) + ' '
 
     asn = sn
     asn = asn.replace('BoldItalic', 'Bold')
     asn = asn.replace('Italic', 'Regular')
-    cmd = '-s ' + str(1/1.4) + ' ' + '-i ' + annapurna_dir + emsize + annapurna_ttf + asn + emext + ' --rangefile cs/annapurna/main.txt'
+    cmd = scale + '-i ' + annapurna + asn + '.ttf' + ' --rangefile cs/annapurna/main.txt'
     modifyFile(cmd, sfd)
 
-    cmd = emopt + '-i ' + charis_dir + '2048' + charis_ttf + s + '.ttf' + ' -n uni0334.Lrg -n uni03A9 --rangefile cs/charis/pre.txt --rangefile cs/charis/main.txt'
+    cmd = scale + '-i ' + charis + s + '.ttf' + ' -n uni0334.Lrg -n uni03A9 --rangefile cs/charis/pre.txt --rangefile cs/charis/main.txt'
     modifyFile(cmd, sfd)
 
 for f in faces:
