46e588815c7cfc1b32e779670e62e09a834c4a93
Bobby de Vos <bobby_devos@sil.org>
2024-04-24 06:55:28 -0600

    Improve punctuation

--- tools/import.bash
+++ tools/import.bash
@@ -5,7 +5,9 @@ style="$2"
 ufo="$3"
 
 devaf="Maurya"
+kndaf="Badami"
 tamlf="Vaigai"
 
 psfcopyglyphs -f --rename rename --unicode usv -i ${cs}/panini/main4gujr.csv -s "${deva}/${devaf}-${style}.ufo" ${ufo}
+psfcopyglyphs -f --rename rename --unicode usv -i ${cs}/badami/main4gujr.csv -s "${knda}/${kndaf}-${style}.ufo" ${ufo}
 psfcopyglyphs -f --rename rename --unicode usv -i ${cs}/thiruvalluvar/main.csv -s "${taml}/${tamlf}-${style}.ufo" ${ufo}
