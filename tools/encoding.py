2689fc069e167c6a1acf32918b69b21626779210
Bobby de Vos <bobby_devos@sil.org>
2025-02-24 16:18:01 -0700

    Simplify glyph names
    Names are now more of what the glyph is,
    rather than how the glyph is formed (lookups).

--- tools/encoding.py
+++ tools/encoding.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
 
-from fontParts.world import *
+import fontParts.world as fontparts
 import sys
 
 # Open UFO
 ufo = sys.argv[1]
-font = OpenFont(ufo)
+font = fontparts.OpenFont(ufo)
 
 # Modify UFO
+for glyph in font:
+    if glyph.name == 'ra_virama':
+        glyph.name = 'reph'
+    elif glyph.name.startswith('ra_umatra'):
+        pass
+    elif glyph.name.startswith('ra_uumatra'):
+        pass
+    elif glyph.name.startswith('ra_'):
+        glyph.name = 'reph_' + glyph.name.removeprefix('ra_') # ligature with reph
+    elif glyph.name.startswith('virama_ra'):
+        glyph.name = glyph.name.replace('virama_ra', 'vattu')
 
 # Save UFO
-# font.changed()
-# font.save()
-# font.close()
+font.changed()
+font.save()
+font.close()
